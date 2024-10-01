c9eb4095d118a8831ff16107a831a5841247e898
Seth Way <way.sethaniel@gmail.com>
2024-10-01 18:56:39 -0400

    simplest svg

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,7 +1,7 @@
 const Headline = ({ firstName }) => {
 	return (
 		<div id="headline" className="flex items-center justify-center font-bold">
-			<svg
+			{/* <svg
 				id="headline-svg"
 				viewBox="0 0 800 100"
 				xmlns="http://www.w3.org/2000/svg"
@@ -15,7 +15,7 @@ const Headline = ({ firstName }) => {
             <stop offset="60%" stop-color="hsl(var(--muted-foreground))" />
             <stop offset="90%" stop-color="var(--primary_C)" />
             <stop offset="100%" stop-color="var(--primary_C)" />
-          </linearGradient> */}
+          </linearGradient>
 				<circle cx="400" cy="50" r="50" />
 				{/* <text
 					// x="50%"
@@ -25,8 +25,16 @@ const Headline = ({ firstName }) => {
 					fontSize="95"
 					fill="white">
 					{firstName}
-				</text> */}
-				{/* <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fontSize = "99" fill="url(#Gradient1)">{firstName}</text> */}
+				</text>
+				{/* <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fontSize = "99" fill="url(#Gradient1)">{firstName}</text>
+			</svg> */}
+			<svg
+				viewBox="0 0 800 100"
+				xmlns="http://www.w3.org/2000/svg"
+				stroke="purple"
+				strokeWidth="2"
+				fill="blue">
+				<circle cx="50" cy="50" r="50" />
 			</svg>
 		</div>
 	);
