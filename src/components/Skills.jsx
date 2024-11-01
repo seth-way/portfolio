9c877c6a687e3647525a1e6a6ed4357fcf99b8c3
Seth Way <way.sethaniel@gmail.com>
2024-10-31 20:00:28 -0400

    update skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,24 +3,19 @@ import { Card } from '@/components/ui/card';
 import skills from '@/assets/resume-info/skills.json';
 
 const Skills = () => {
-  return (
-    <Card
-      id='skills'
-      className='section-card px-4 py-8 flex flex-col items-center justify-center gap-8'
-    >
-      {' '}
-      <h2>skills</h2>
-      <div className='flex items-center justify-center flex-wrap max-w-[min(600px,85vw)] gap-[clamp(0.5rem,1.5vw,1rem)]'>
-        {skills.map((skill, idx) => (
-          <SkillText
-            skillInfo={skill}
-            idx={idx}
-            key={`${idx}_${skill.skill}`}
-          />
-        ))}
-      </div>
-    </Card>
-  );
+	return (
+		<Card
+			id="skills"
+			className="section-card px-4 py-8 flex flex-col items-center justify-center gap-8">
+			{' '}
+			<h2>skills</h2>
+			<div className="flex items-center justify-center flex-wrap max-w-[min(800px,85vw)] gap-[clamp(0.5rem,1.5vw,1rem)]">
+				{skills.map((skill, idx) => (
+					<SkillText skillInfo={skill} idx={idx} key={`${idx}_${skill.skill}`} />
+				))}
+			</div>
+		</Card>
+	);
 };
 
 export default Skills;
