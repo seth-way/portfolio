829beb9c28bd22ab1b62deed6076249a18100855
Seth Way <way.sethaniel@gmail.com>
2024-11-09 20:08:03 -0500

    refactor skills using skill groups

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,17 +1,31 @@
+import { motion } from 'framer-motion';
 import SkillText from '@/components/Skill';
 import { Card } from '@/components/ui/card';
-import skills from '@/assets/resume-info/skills.json';
+import skillGroups from '@/assets/resume-info/skills.json';
 
 const Skills = () => {
 	return (
 		<Card
 			id="skills"
-			className="section-card px-4 py-8 flex flex-col items-center justify-center gap-8">
-			{' '}
+			className="section-card px-4 py-8 flex flex-col items-center justify-center gap-8 overflow-hidden">
 			<h2>skills</h2>
-			<div className="flex items-center justify-center flex-wrap max-w-[min(800px,85vw)] gap-[clamp(0.5rem,1.5vw,1rem)]">
-				{skills.map((skill, idx) => (
-					<SkillText skillInfo={skill} idx={idx} key={`${idx}_${skill.skill}`} />
+			<div className="flex items-center justify-center flex-wrap max-w-[min(800px,85vw)] gap-[clamp(2rem,1.75vw,2.5rem)]">
+				{skillGroups.map(({ category, skills }, idx) => (
+					<div key={`category_${category}_${idx}`} className="flex flex-col items-center">
+						<motion.h3
+							initial={{ x: '-100%' }}
+							transition={{ duration: 1, type: 'spring' }}
+							whileInView={{ x: '0%' }}
+							viewport={{ once: true }}
+							className="text-3xl border-2 border-solid italic text-[hsl(var(--muted-foreground))] font-extrabold py-2 px-4">
+							{category}
+						</motion.h3>
+						<div className="flex items-center justify-center flex-wrap gap-x-[clamp(0.5rem,1.5vw,1rem)]">
+							{skills.map((skill, idx2) => (
+								<SkillText skillInfo={skill} idx={idx2} key={`${idx2}_${skill.skill}`} />
+							))}
+						</div>
+					</div>
 				))}
 			</div>
 		</Card>
@@ -19,3 +33,7 @@ const Skills = () => {
 };
 
 export default Skills;
+
+function getSkillsValue(skills) {
+	return skills.reduce((total, { value }) => total + value, 0);
+}
