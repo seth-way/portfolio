5c3f8460d0d7c8244e81c1ceb24a10b79c441989
Seth Way <way.sethaniel@gmail.com>
2024-11-23 16:30:50 -0500

    create initial jurassic animations

---
+++ src/components/misc/svgs/JokeSVG.jsx
@@ -0,0 +1,21 @@
+import AIMSVG from '@/components/misc/svgs/AIM';
+import BlockbusterSVG from '@/components/misc/svgs/BlockBuster';
+import JurassicSVG from '@/components/misc/svgs/Jurassic';
+const JokeSVG = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 300 200'
+      fill='none'
+      strokeLinecap='round'
+      height='100%'
+      width='100%'
+    >
+      <BlockbusterSVG />
+      <AIMSVG />
+      <JurassicSVG />
+    </svg>
+  );
+};
+
+export default JokeSVG;
