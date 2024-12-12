a1d30e7224c6a59f3711e1c74f4a48d6470f2cb2
Seth Way <way.sethaniel@gmail.com>
2024-12-12 13:38:40 -0500

    brighten airborne op card

--- src/components/misc/Airborne.jsx
+++ src/components/misc/Airborne.jsx
@@ -5,82 +5,72 @@ import AirborneOp from '@/components/misc/svgs/AirborneOp';
 const TOTAL_JUMPS = 43;
 
 const Airborne = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [bgColor, setBgColor] = useState('bg-transparent');
+	const [isOpen, setIsOpen] = useState(false);
+	const [bgColor, setBgColor] = useState('bg-transparent');
 
-  useEffect(() => {
-    if (!isOpen) {
-      setBgColor('bg-transparent');
-      return;
-    }
+	useEffect(() => {
+		if (!isOpen) {
+			setBgColor('bg-transparent');
+			return;
+		}
 
-    let timeoutId;
-    let intervalId;
+		let timeoutId;
+		let intervalId;
 
-    const flashBackground = () => {
-      const colors =
-        Math.random() > 0.5
-          ? [
-              'bg-yellow-100/10',
-              'bg-black/20',
-              'bg-yellow-100/10',
-              'bg-black/20',
-              'bg-yellow-100/10',
-              'bg-transparent',
-            ]
-          : [
-              'bg-yellow-100/10',
-              'bg-black/20',
-              'bg-yellow-100/10',
-              'bg-transparent',
-            ];
-      let currentIndex = 0;
+		const flashBackground = () => {
+			const colors =
+				Math.random() > 0.5
+					? [
+							'bg-yellow-100/10',
+							'bg-black/20',
+							'bg-yellow-100/10',
+							'bg-black/20',
+							'bg-yellow-100/10',
+							'bg-transparent'
+					  ]
+					: ['bg-yellow-100/10', 'bg-black/20', 'bg-yellow-100/10', 'bg-transparent'];
+			let currentIndex = 0;
 
-      intervalId = setInterval(() => {
-        setBgColor(colors[currentIndex]);
-        currentIndex += 1;
+			intervalId = setInterval(() => {
+				setBgColor(colors[currentIndex]);
+				currentIndex += 1;
 
-        if (currentIndex >= colors.length) {
-          clearInterval(intervalId);
-        }
-      }, Math.random() * 75 + 95); // 0.2 seconds per color
+				if (currentIndex >= colors.length) {
+					clearInterval(intervalId);
+				}
+			}, Math.random() * 75 + 95); // 0.2 seconds per color
 
-      // Schedule the next flash after a random delay
-      const nextFlash = Math.random() * 4000 + 4000;
-      timeoutId = setTimeout(flashBackground, nextFlash);
-    };
+			// Schedule the next flash after a random delay
+			const nextFlash = Math.random() * 4000 + 4000;
+			timeoutId = setTimeout(flashBackground, nextFlash);
+		};
 
-    flashBackground();
+		flashBackground();
 
-    return () => {
-      clearTimeout(timeoutId); // Clear the timeout when unmounting
-      clearInterval(intervalId); // Clear any active interval
-    };
-  }, [isOpen]);
+		return () => {
+			clearTimeout(timeoutId); // Clear the timeout when unmounting
+			clearInterval(intervalId); // Clear any active interval
+		};
+	}, [isOpen]);
 
-  const handleOpen = () => {
-    if (!isOpen) setIsOpen(true);
-  };
+	const handleOpen = () => {
+		if (!isOpen) setIsOpen(true);
+	};
 
-  const handleClose = e => {
-    e.stopPropagation();
-    setIsOpen(false);
-  };
+	const handleClose = e => {
+		e.stopPropagation();
+		setIsOpen(false);
+	};
 
-  return (
-    <div
-      onClick={handleOpen}
-      className='h-full w-full relative bg-gradient-to-b from-gray-900 via-gray-800 via-80% to-gray-700 overflow-hidden'
-    >
-      <Clouds isOpen={isOpen} jumps={TOTAL_JUMPS} />
-      <AirborneOp
-        isOpen={isOpen}
-        handleClose={handleClose}
-        jumps={TOTAL_JUMPS}
-      />
-      <div className={`absolute inset-0 blur-lg overflow-hidden ${bgColor}`} />
-    </div>
-  );
+	return (
+		<div
+			onClick={handleOpen}
+			className="h-full w-full relative bg-gradient-to-b from-gray-800 via-gray-700 via-80% to-gray-600 overflow-hidden">
+			<Clouds isOpen={isOpen} jumps={TOTAL_JUMPS} />
+			<AirborneOp isOpen={isOpen} handleClose={handleClose} jumps={TOTAL_JUMPS} />
+			<div className={`absolute inset-0 blur-lg overflow-hidden ${bgColor}`} />
+		</div>
+	);
 };
 
 export default Airborne;
