aa5acd93386d0729ff76c584a3e1b55c66bb673d
Seth Way <way.sethaniel@gmail.com>
2025-01-12 21:07:56 -0500

    refactor dot animation

--- src/components/Dot.jsx
+++ src/components/Dot.jsx
@@ -1,23 +1,49 @@
-import { useRef, memo } from 'react';
+import { useState, useEffect, useRef, useMemo, memo } from 'react';
 import { motion } from 'motion/react';
 
-const Dot = memo(({ deltaX, deltaY, scale }) => {
-  const delay = useRef(Math.random() + 0.01);
+const Dot = memo(({ deltaX, deltaY, scale, size }) => {
+	const delay = useRef(Math.random() * 4 + 0.01);
+	const initialLocation = useRef({ x: getRandomDelta(size), y: getRandomDelta(size) });
+	const location = useMemo(
+		() => ({ x: initialLocation.current.x + deltaX, y: initialLocation.current.y + deltaY }),
+		[deltaX, deltaY]
+	);
 
-  return (
-    <div className='w-4 h-4 relative'>
-      <motion.div
-        className='absolute inset-[40%] bg-foreground rounded-full'
-        initial={false}
-        animate={{ x: deltaX, y: deltaY, scale: scale }}
-        transition={{ type: 'spring', stiffness: 850, damping: 10, mass: 0.15 }}
-        style={{
-          opacity: 0.3,
-          animation: `dots 1s ease-in-out ${delay.current}s infinite alternate`,
-        }}
-      ></motion.div>
-    </div>
-  );
+	const [localScale, setLocalScale] = useState(1);
+
+	useEffect(() => {
+		if (scale > localScale) {
+			setLocalScale(scale);
+		}
+	}, [scale, localScale]);
+
+	useEffect(() => {
+		if (localScale > 1) {
+			const interval = setInterval(() => {
+				setLocalScale(prev => (prev > 1.1 ? prev * 0.85 : 1));
+			}, 100);
+
+			return () => clearInterval(interval);
+		}
+	}, [localScale]);
+
+	return (
+		<div className="relative">
+			<motion.div
+				className="absolute inset-[45%] bg-foreground rounded-full"
+				initial={false}
+				animate={{ x: location.x || 0, y: location.y || 0, scale: localScale }}
+				transition={{ type: 'spring', stiffness: 850, damping: 10, mass: 0.15 }}
+				style={{
+					opacity: 0.3,
+					animation: `dots 2s ease-in-out ${delay.current}s infinite alternate`
+				}}></motion.div>
+		</div>
+	);
 });
 
 export default Dot;
+
+function getRandomDelta(size) {
+	return ((Math.random() * size) / 2) * (Math.random() > 0.5 ? 1 : -1);
+}
--- src/components/DotsBG.jsx
+++ src/components/DotsBG.jsx
@@ -2,121 +2,122 @@ import { useState, useEffect, useRef, useMemo } from 'react';
 import { useWindowSize } from '@react-hook/window-size/throttled';
 import Dot from '@/components/Dot';
 
-const SQUARE_SIZE = 22;
+const SQUARE_SIZE = 40;
 const MIN_DIST = 10;
 const MID_DIST = 100;
 const MAX_DIST = 130;
 const DEFAULT_DOT = { deltaX: 0, deltaY: 0, scale: 1 };
 
 export default function DotsBG() {
-  const [cursorPos, setCursorPos] = useState(null);
-  const [grid, setGrid] = useState({ rows: 0, cols: 0 });
-  const [dotLocations, setDotLocations] = useState([]);
-  const bgRef = useRef(null);
-  const [height, width] = useWindowSize();
+	const [cursorPos, setCursorPos] = useState(null);
+	const [grid, setGrid] = useState({ rows: 0, cols: 0 });
+	const [dotLocations, setDotLocations] = useState([]);
+	const bgRef = useRef(null);
+	const [height, width] = useWindowSize();
 
-  useEffect(() => {
-    if (bgRef.current) {
-      const { height, width } = bgRef.current.getBoundingClientRect();
-      setGrid({
-        rows: Math.round(height / SQUARE_SIZE),
-        cols: Math.round(width / SQUARE_SIZE),
-      });
-    }
-  }, [height, width]);
+	useEffect(() => {
+		if (bgRef.current) {
+			const { height, width } = bgRef.current.getBoundingClientRect();
+			setGrid({
+				rows: Math.round(height / SQUARE_SIZE),
+				cols: Math.round(width / SQUARE_SIZE)
+			});
+		}
+	}, [height, width]);
 
-  useEffect(() => {
-    if (bgRef.current) {
-      const { height, width } = bgRef.current.getBoundingClientRect();
-      const locations = [];
+	useEffect(() => {
+		if (bgRef.current) {
+			const { height, width } = bgRef.current.getBoundingClientRect();
+			const locations = [];
 
-      for (let row = 0; row < grid.rows; row++) {
-        for (let col = 0; col < grid.cols; col++) {
-          locations.push({
-            x: (col + 0.5) * (width / grid.cols),
-            y: (row + 0.5) * (height / grid.rows),
-          });
-        }
-      }
-      setDotLocations(locations);
-    }
-  }, [grid]);
+			for (let row = 0; row < grid.rows; row++) {
+				for (let col = 0; col < grid.cols; col++) {
+					locations.push({
+						x: (col + 0.5) * (width / grid.cols),
+						y: (row + 0.5) * (height / grid.rows)
+					});
+				}
+			}
+			setDotLocations(locations);
+		}
+	}, [grid]);
 
-  const dots = useMemo(() => {
-    if (!cursorPos) {
-      return dotLocations.map(() => DEFAULT_DOT);
-    }
+	const dots = useMemo(() => {
+		if (!cursorPos) {
+			return dotLocations.map(() => DEFAULT_DOT);
+		}
 
-    const { left, top } = bgRef.current.getBoundingClientRect();
+		const { left, top } = bgRef.current.getBoundingClientRect();
 
-    return dotLocations.map(({ x, y }) => {
-      const dx = cursorPos.x - left - x;
-      const dy = cursorPos.y - top - y;
+		return dotLocations.map(({ x, y }) => {
+			const dx = cursorPos.x - left - x;
+			const dy = cursorPos.y - top - y;
 
-      return animateDot(dx, dy);
-    });
-  }, [cursorPos?.x, cursorPos?.y, dotLocations]);
+			return animateDot(dx, dy);
+		});
+	}, [cursorPos?.x, cursorPos?.y, dotLocations]);
 
-  function animateDot(dx, dy) {
-    const dist = Math.sqrt(dx ** 2 + dy ** 2);
+	function animateDot(dx, dy) {
+		const approxDist = Math.abs(dx) + Math.abs(dy);
 
-    if (dist < MAX_DIST) {
-      let deltaX = 0;
-      let deltaY = 0;
-      const scale = ((MAX_DIST - dist) / MAX_DIST) * 5;
+		if (approxDist < MAX_DIST) {
+			let deltaX = 0;
+			let deltaY = 0;
 
-      if (dist > MIN_DIST) {
-        const angle = Math.atan2(dy, dx);
-        let strength, scaleFactor;
-        if (dist < MID_DIST) {
-          scaleFactor = -20;
-          strength = (MID_DIST - dist) / MID_DIST;
-        } else {
-          scaleFactor = 20;
-          strength = (dist - MID_DIST) / (MAX_DIST - MID_DIST);
-        }
-        deltaX = Math.cos(angle) * strength * scaleFactor;
-        deltaY = Math.sin(angle) * strength * scaleFactor;
-      }
+			const scale = ((MAX_DIST - approxDist) / MAX_DIST) * 8;
 
-      return {
-        deltaX,
-        deltaY,
-        scale,
-      };
-    }
+			if (approxDist > MIN_DIST) {
+				let strength, scaleFactor;
+				if (approxDist < MID_DIST) {
+					scaleFactor = -0.1;
+					strength = (MID_DIST - approxDist) / MID_DIST;
+				} else {
+					scaleFactor = 0.1;
+					strength = (approxDist - MID_DIST) / (MAX_DIST - MID_DIST);
+				}
 
-    return DEFAULT_DOT;
-  }
+				deltaX = dx * strength * scaleFactor;
+				deltaY = dy * strength * scaleFactor;
+			}
 
-  const handleMouseMove = event => {
-    setCursorPos({ x: event.clientX, y: event.clientY });
-  };
+			return {
+				deltaX,
+				deltaY,
+				scale
+			};
+		}
 
-  const handleMouseLeave = () => {
-    setCursorPos(null);
-  };
+		return DEFAULT_DOT;
+	}
 
-  return (
-    <div
-      ref={bgRef}
-      className='absolute inset-0 pointer-events-auto touch:pointer-events-none'
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-      style={{
-        display: 'grid',
-        gridTemplateRows: `repeat(${grid.rows}, 1fr)`,
-        gridTemplateColumns: `repeat(${grid.cols}, 1fr)`,
-      }}
-    >
-      {dots.map(({ deltaX, deltaY, scale }, index) => (
-        <Dot
-          key={'dot-' + index}
-          deltaX={deltaX}
-          deltaY={deltaY}
-          scale={scale}
-        />
-      ))}
-    </div>
-  );
+	const handleMouseMove = event => {
+		setCursorPos({ x: event.clientX, y: event.clientY });
+	};
+
+	const handleMouseLeave = () => {
+		setCursorPos(null);
+	};
+
+	return (
+		<div
+			ref={bgRef}
+			className="absolute inset-0 pointer-events-auto touch:pointer-events-none"
+			onMouseMove={handleMouseMove}
+			onMouseLeave={handleMouseLeave}
+			style={{
+				display: 'grid',
+				gridTemplateRows: `repeat(${grid.rows}, 1fr)`,
+				gridTemplateColumns: `repeat(${grid.cols}, 1fr)`
+			}}>
+			{dots.map(({ deltaX, deltaY, scale }, index) => (
+				<Dot
+					key={'dot-' + index}
+					deltaX={deltaX}
+					deltaY={deltaY}
+					scale={scale}
+					size={SQUARE_SIZE}
+				/>
+			))}
+		</div>
+	);
 }
