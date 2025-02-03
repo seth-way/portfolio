a2f636fb5b1d882c310d300b8c8d7c7415e03515
Seth Way <way.sethaniel@gmail.com>
2025-02-03 13:53:04 -0500

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from './components/ui/theme-provider.jsx';
+import { inject } from '@vercel/analytics';
 import App from './App.jsx';
 import './index.css';
 
+inject();
+
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-			<App />
-		</ThemeProvider>
-	</StrictMode>
+  <StrictMode>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 );
